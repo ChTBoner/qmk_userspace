56618f32ff765d9fe78f91496ea2d89b8fbdb812
Thibault Delattre <thibothekoala@yahoo.fr>
2024-11-24 09:21:59 +0100

    fix

--- layouts/split_3x6_3/chtpwner/config.h
+++ layouts/split_3x6_3/chtpwner/config.h
@@ -19,7 +19,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #pragma once
-
+#define LED_CAPS_LOCK_PIN 24
+#define LED_PIN_ON_STATE 0
 //#define USE_MATRIX_I2C
 
 //#define QUICK_TAP_TERM 0
